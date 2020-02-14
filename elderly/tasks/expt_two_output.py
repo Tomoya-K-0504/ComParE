d1583e5d84dde821f85742f3b4d716991a5a19fd
Tomoya Koike <makeffort134@gmail.com>
2020-02-14 23:38:06 +0900

    Multitask training added

--- elderly/tasks/expt_two_output.py
+++ elderly/tasks/expt_two_output.py
@@ -16,16 +16,12 @@ LABELS2INT = {'L': 0, 'M': 1, 'H': 2}
 def elderly_expt_args(parser):
     parser = base_expt_args(parser)
     expt_parser = parser.add_argument_group("Elderly Experiment arguments")
-    expt_parser.add_argument('--target', help='Valence or arousal', choices=['valence', 'arousal'], default='valence')
 
     return parser
 
 
-def set_label_func(target_col):
-    def label_func(row):
-        return row[target_col]
-
-    return label_func
+def label_func(row):
+    return row[5], row[6]
 
 
 def set_load_func(data_dir, sr):
@@ -52,7 +48,7 @@ def set_load_func(data_dir, sr):
 
 
 def train_with_all(val_results, hyperparameter_list, expt_conf, experimentor):
-    best_trial_idx = val_results['uar'].argmax()
+    best_trial_idx = (val_results['v_uar'] + val_results['a_uar']).argmax()
     best_pattern = patterns[best_trial_idx]
 
     for idx, param in enumerate(hyperparameter_list):
@@ -68,32 +64,39 @@ def train_with_all(val_results, hyperparameter_list, expt_conf, experimentor):
 
     pred = experimentor.experiment_without_validation()
 
-    sub_name = f"{expt_conf['expt_id']}_{'_'.join(list(map(str, best_pattern)))}.csv"
+    sub_name = f"{expt_conf['expt_id']}_{'_'.join([str(p).replace('/', '-') for p in best_pattern])}.csv"
     if (Path(__file__).resolve().parents[1] / 'output' / 'sub' / sub_name).is_file():
         sub_df = pd.read_csv(Path(__file__).resolve().parents[1] / 'output' / 'sub' / sub_name)
     else:
-        sub_df = manifest_df[manifest_df['partition'] == 'test']['filename_text']
+        sub_df = manifest_df[manifest_df['partition'] == 'test'][['filename_text']]
         (Path(__file__).resolve().parents[1] / 'output' / 'sub').mkdir(exist_ok=True)
-
-    sub_df[expt_conf['target']] = pd.Series(pred).apply(lambda x: list(LABELS2INT.keys())[x])
+    for i, target in enumerate(expt_conf['target']):
+        sub_df[target] = pd.Series(pred[:, i]).apply(lambda x: list(LABELS2INT.keys())[x])
     sub_df.to_csv(Path(__file__).resolve().parents[1] / 'output' / 'sub' / sub_name, index=False)
+    print(f"Submission file is saved in {Path(__file__).resolve().parents[1] / 'output' / 'sub' / sub_name}")
 
 
-def main(expt_conf):
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='train arguments')
+    expt_conf = vars(elderly_expt_args(parser).parse_args())
+    assert expt_conf['train_path'] != '' or expt_conf['val_path'] != '', \
+        'You need to select training, validation data file to training, validation in --train-path, --val-path argments'
+
     hyperparameters = {
-        'target': ['valence', 'arousal'],
-        'window_size': [0.04, 0.08, 0.12],
-        'window_stride': [0.02, 0.04, 0.06]
+        'model_type': ['multitask_panns'],
+        'target': [['valence', 'arousal']],
+        'checkpoint_path': ['../cnn14.pth'],
+        'window_size': [0.08],
+        'window_stride': [0.02]
     }
 
     expt_conf['class_names'] = [0, 1, 2]
     expt_conf['sample_rate'] = 16000
+    expt_conf['n_tasks'] = 2
 
     load_func = set_load_func(Path(expt_conf['manifest_path']).resolve().parents[1] / 'wav', expt_conf['sample_rate'])
-    target_col = 5 if expt_conf['target'] == 'valence' else 6
-    label_func = set_label_func(target_col)
-
-    val_metrics = ['loss', 'uar']
+    val_metrics = ['loss', 'uar', 'loss', 'uar']
+    val_metrics_columns = ['v_loss', 'v_uar', 'a_loss', 'a_uar']
 
     manifest_df = pd.read_csv(expt_conf['manifest_path'])
     for phase, part in zip(['train', 'val', 'infer'], ['train', 'devel', 'test']):
@@ -102,9 +105,9 @@ def main(expt_conf):
         expt_conf[f'{phase}_path'] = str(Path(expt_conf['manifest_path']).parent / f'{phase}_manifest.csv')
 
     patterns = list(itertools.product(*hyperparameters.values()))
-    patterns = [(w_size, w_stride) for w_size, w_stride in patterns if w_size > w_stride]
-    val_results = pd.DataFrame(np.zeros((len(patterns), len(hyperparameters) + len(val_metrics))),
-                               columns=list(hyperparameters.keys()) + val_metrics)
+    # patterns = [(w_size, w_stride) for w_size, w_stride in patterns if w_size > w_stride]
+    val_results = pd.DataFrame(np.zeros((len(patterns), len(hyperparameters) + len(val_metrics_columns))),
+                               columns=list(hyperparameters.keys()) + val_metrics_columns)
 
     for i, pattern in enumerate(patterns):
         val_results.iloc[i, :len(hyperparameters)] = pattern
@@ -140,9 +143,8 @@ def main(expt_conf):
             mlflow.log_params({hyperparameter: value for hyperparameter, value in zip(hyperparameters.keys(), pattern)})
             mlflow.log_metrics({metric_name: value for metric_name, value in zip(val_metrics, result_series)})
 
-    (Path(__file__).resolve().parents[1] / 'output' / 'metrics' / expt_conf['target']).mkdir(exist_ok=True)
-    expt_path = Path(__file__).resolve().parents[1] / 'output' / 'metrics' / expt_conf[
-        'target'] / f"{expt_conf['expt_id']}.csv"
+    (Path(__file__).resolve().parents[1] / 'output' / 'metrics').mkdir(exist_ok=True)
+    expt_path = Path(__file__).resolve().parents[1] / 'output' / 'metrics' / f"{expt_conf['expt_id']}.csv"
     print(val_results)
     print(val_results.iloc[:, len(hyperparameters):].describe())
     val_results.to_csv(expt_path, index=False)
@@ -150,9 +152,3 @@ def main(expt_conf):
     # Train with train + devel dataset
     if expt_conf['train_with_all']:
         train_with_all(val_results, list(hyperparameters.keys()), expt_conf, experimentor)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='train arguments')
-    expt_conf = vars(elderly_expt_args(parser).parse_args())
-    main(expt_conf)
